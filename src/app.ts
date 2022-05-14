b0209ef93dc947cd61ff6affd157399d62b6cd7b
Simon Plenderleith <104832360+simonteaching@users.noreply.github.com>
2022-05-14 17:08:24 +0100

    Add GET /planets route

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import "express-async-errors";
 
 const app = express();
 
-app.get("/", (request, response) => {
-    response.send("This is the Space Facts API!");
+app.get("/planets", (request, response) => {
+    response.json([
+        { name: "Mercury" },
+        { name: "Venus" }
+    ]);
 });
 
 export default app;
