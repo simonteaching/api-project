c849e89fad302a8d365bea4f0784e22535a64630
Simon Plenderleith <104832360+simonteaching@users.noreply.github.com>
2022-06-16 11:05:04 +0100

    Move all configuration into one script

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import "dotenv/config";
 
+import config from "./config";
+
 import app from "./app";
 
-const port = process.env.PORT;
+const port = config.PORT;
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`)
