3881662ccf0ae56073a9bead3ce84d98d391579e
Simon Plenderleith <simon@simonplend.co.uk>
2022-06-17 15:33:21 +0100

    Configure sessions for production

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ import authRoutes from "./routes/auth";
 
 const app = express();
 
-app.use(initSessionMiddleware());
+app.use(initSessionMiddleware(app.get("env")));
 app.use(passport.initialize());
 app.use(passport.session());
 
